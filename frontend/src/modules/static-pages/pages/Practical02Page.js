07eccc23cfa20ae8224fbceea0f91586e04fa639
Martin Kapal <flamecze@gmail.com>
2022-09-27 18:41:50 +0200

    Practical 02 end

--- frontend/src/modules/static-pages/pages/Practical02Page.js
+++ frontend/src/modules/static-pages/pages/Practical02Page.js
@@ -1,19 +1,44 @@
+import { useState } from 'react';
 import { useTodoList } from 'src/modules/todo/hooks';
 import { Box, Button, Heading, Input } from 'src/shared/design-system';
 
 export function Practical02Page() {
-  const { items } = useTodoList();
+  const { items, addItem, setItemCompleted } = useTodoList();
+  const [newItemName, setNewItemName] = useState('');
 
   return (
     <Box>
       <Heading>Practical 02</Heading>
-      <Input type="text" placeholder="What needs to be done?" />
-      <Button type="submit">Add</Button>
+      <form
+        onSubmit={(event) => {
+          // Prevent reloading the page on submit (default browser behaviour)
+          event.preventDefault();
+
+          // Add the item to the to-do list state
+          addItem(newItemName);
+
+          // Clear the input field
+          setNewItemName('');
+        }}
+      >
+        <Input
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+          type="text"
+          placeholder="What needs to be done?"
+        />
+        <Button type="submit">Add</Button>
+      </form>
       <ul>
         {items.map((item) => (
           <li key={item.id}>
             <label>
-              <input type="checkbox" /> {item.name}
+              <input
+                type="checkbox"
+                onChange={() => setItemCompleted(item.id, !item.isCompleted)}
+                checked={item.isCompleted}
+              />{' '}
+              {item.name}
             </label>
           </li>
         ))}
