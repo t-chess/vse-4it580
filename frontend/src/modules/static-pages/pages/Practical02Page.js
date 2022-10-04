3a709e54af5ba8f24d328e23ac97e7bfb608fdfe
Chesebieva Tatiana <chet01@vse.cz>
2022-10-04 17:22:38 +0200

    ukol2

--- frontend/src/modules/static-pages/pages/Practical02Page.js
+++ frontend/src/modules/static-pages/pages/Practical02Page.js
@@ -1,23 +1,56 @@
+import { useEffect, useState } from 'react';
 import { useTodoList } from 'src/modules/todo/hooks';
 import { Box, Button, Heading, Input } from 'src/shared/design-system';
+import {DeleteIcon} from '@chakra-ui/icons'; 
+import { Checkbox, HStack, Tab, TabList, Tabs } from '@chakra-ui/react';
 
 export function Practical02Page() {
-  const { items } = useTodoList();
+  const { items, addItem, setItemCompleted, deleteItem } = useTodoList();
+  const [newItemName, setNewItemName] = useState('');
+  const [filteredItems, setFilteredItems] = useState(items)
+
+  const STATES = ['all', 'completed', 'not-completed'];
+  const [filter, setFilter] = useState('all');
+
+  useEffect(() => {
+      setFilteredItems(()=>{
+        if (filter=='all') {return items};
+        if (filter=='completed') {return items.filter(i=>i.isCompleted)};
+        if (filter=='not-completed') {return items.filter(i=>!i.isCompleted)};
+      })
+  },[filter])
 
   return (
     <Box>
       <Heading>Practical 02</Heading>
-      <Input type="text" placeholder="What needs to be done?" />
-      <Button type="submit">Add</Button>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <label>
-              <input type="checkbox" /> {item.name}
-            </label>
-          </li>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        addItem(newItemName);
+        setNewItemName('');
+      }}>
+        <Input value={newItemName} onChange={(e)=>{setNewItemName(e.target.value)}} type="text" placeholder="What needs to be done?" />
+        <Button marginBottom='25px' type="submit">Add</Button>
+      </form>
+      <Tabs
+        index={STATES.indexOf(filter)}
+        onChange={(index) => setFilter(STATES[index])}
+        variant="soft-rounded"
+        colorScheme="blue"
+      >
+        <TabList>
+          <Tab>All</Tab>
+          <Tab>Completed</Tab>
+          <Tab>Not completed</Tab>
+        </TabList>
+      </Tabs>
+      <Box w='500px' border='1px' borderColor='gray.300' borderRadius='lg' p='10px' marginTop='25px'>
+        {filteredItems.map((item) => (
+          <HStack _hover={{bg:'gray.100'}} role="group" align='center' w='100%' justify='space-between' px='5px' py='10px' key={item.id}>
+            <Checkbox as={item.isCompleted ? 'del' : ''} onChange={()=>{setItemCompleted(item.id, !item.isCompleted)}} key={item.id} checked={item.isCompleted}>{item.name}</Checkbox> 
+            <DeleteIcon opacity='0%' _groupHover={{opacity:'100%'}} bg='red.500' w='30px' h='30px' p='7px' borderRadius='5px' color='white' onClick={()=>{deleteItem(item.id)}} ></DeleteIcon>
+          </HStack>
         ))}
-      </ul>
+      </Box>
     </Box>
   );
 }
--- frontend/src/modules/todo/hooks/useTodoList.js
+++ frontend/src/modules/todo/hooks/useTodoList.js
@@ -1,35 +1,68 @@
-const items = [
+import { useState } from "react";
+
+const initialItems = [
   {
     id: 1,
     name: 'go grocery shopping',
+    isCompleted: false,
   },
   {
     id: 2,
     name: 'wash the dishes',
+    isCompleted: false,
   },
   {
     id: 3,
     name: 'write some React code',
+    isCompleted: false,
   },
 ];
 
 export function useTodoList() {
-  const addItem = () => {
-    // TODO
+  const [todoState, setTodoState] = useState({
+    items: initialItems,
+    nexId: initialItems.length + 1,
+  });
+  const addItem = (name) => {
+    setTodoState((prevState)=> {
+      const {nextId} = prevState;
+      return {
+        ...prevState,
+        items: [
+          ...prevState.items,
+          {id: nextId, name}
+        ],
+        nexId: nextId + 1
+      }
+    })
   };
 
-  const setItemCompleted = () => {
-    // TODO
+  const setItemCompleted = (id, isCompleted) => {
+    setTodoState((prevState)=> ({
+      ...prevState,
+      items: prevState.items.map((item)=>{
+        if (item.id===id){
+          return {
+            ...item,
+            isCompleted,
+          }
+        }
+        return item
+      })
+    }))
   };
 
-  const removeCompletedItems = () => {
-    // TODO
-  };
+  const deleteItem = (id) => {
+    setTodoState((prevState)=>({
+      ...prevState,
+      items: prevState.items.filter(i => i.id !== id)
+    }))
+  }
 
   return {
-    items,
+    items: todoState.items,
     addItem,
     setItemCompleted,
-    removeCompletedItems,
+    deleteItem,
   };
 }
